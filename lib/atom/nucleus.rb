fbaae319f00b57e50b299bbd553913b1eaebba24
Alexander Popov <alex.wayfer@gmail.com>
2015-08-28 19:05:02 +0300

    Add HTTP methods, closes #1

--- lib/atom/nucleus.rb
+++ lib/atom/nucleus.rb
@@ -1,7 +1,18 @@
 module Atom
 	## Core class, like Framework::Application
 	class Nucleus
-		## Helpers for variables
+		## Framework configuration
+		def self.config
+			@config ||= {
+				request_methods: [:GET, :POST, :PUT, :DELETE]
+			}
+		end
+
+		def config
+			self.class.config
+		end
+
+		## Helpers for request variables
 		def status(value = nil)
 			@status ||= 200
 			value ? @status = value : @status
@@ -22,10 +33,11 @@ module Atom
 		## Init function
 		def call(env)
 			request(env)
-			block = find_block
-			if block
+			route = find_route
+			if route
 				status 200
-				body = instance_exec(*@args, &block)
+				params.merge!(@args)
+				body = instance_exec(*@args.values, &route[:block])
 				[status, headers, [body]]
 			else
 				[404, {}, ['Not Found']]
@@ -33,15 +45,17 @@ module Atom
 		end
 
 		## Functions for routing
-		def self.route(path, &block)
-			routes[path] = block
+		def self.method_missing(sym, *args, &block)
+			request_method = sym.upcase
+			return super unless config[:request_methods].include? request_method
+			routes[request_method] << { path: args[0], block: block }
 		end
 
 		private
 
 		## Helpers for private variables
 		def self.routes
-			@routes ||= {}
+			@routes ||= config[:request_methods].inject({}) { |a, e| a.merge(e => []) }
 		end
 
 		def routes
@@ -49,38 +63,41 @@ module Atom
 		end
 
 		## Find block of code for routing
-		def find_block
-			routes[find_route_key]
-		end
-
-		def find_route_key
-			routes.keys.detect do |route_path|
-				@args = []
-				case route_path.class
-				when Regexp
-					request.path_info =~ route_path
-				else
-					compare_paths(request.path_info, route_path.to_s)
-				end
+		def find_route
+			params_method = params['_method']
+			if params_method
+				request_method = params_method.upcase.to_sym
+			else
+				request_method = request.request_method.to_sym
+			end
+			routes[request_method].find do |route|
+				@args = {}
+				compare_paths(request.path_info, route[:path])
 			end
 		end
 
-		def compare_paths(route_path)
-			path_parts = route_path.split('/').reject(&:empty?)
-			request_parts = request.path_info.split('/').reject(&:empty?)
-			return false if request_parts.empty?
+		def compare_paths(request_path, route_path)
+			case route_path.class
+			when Regexp
+				request_path =~ route_path
+			else
+				path_parts = route_path.to_s.split('/').reject(&:empty?)
+				request_parts = request_path.split('/').reject(&:empty?)
+				return false if request_parts.count != path_parts.count
+				compare_parts(request_parts, path_parts)
+			end
+		end
+
+		def compare_parts(request_parts, path_parts)
 			request_parts.each_with_index do |request_part, i|
-				compare_parts(request_part, path_parts[i])
+				path_part = path_parts[i]
+				break false unless path_part
+				if path_part[0] == ':'
+					@args[path_part[1..-1].to_sym] = URI.decode(request_part)
+					next
+				end
+				break false unless request_part == path_part
 			end
-		end
-
-		def compare_parts(request_part, path_part)
-			break false unless path_part
-			if path_part[0] == ':'
-				@args << URI.decode(request_part)
-				next
-			end
-			break false unless request_part == path_part
 		end
 	end
 end
--- my_app.rb
+++ my_app.rb
@@ -2,17 +2,46 @@
 class MyApp < Atom::Nucleus
 	puts '-- Init MyApp'
 
-	route :hello do
-		p ':hello'
-		"Hello #{params['name'] || 'World'}!"
+	get '/' do
+		'index'
 	end
 
-	route '/hello/:name' do |name|
-		p '/hello/:name'
+	get '/users' do
+		'Index of users' \
+		'<form action="/users" method="POST">' \
+			'<input type="submit" value="Create" />' \
+		'</form>'
+	end
+
+	post '/users' do
+		'Create new user'
+	end
+
+	get '/users/:id' do |id|
+		"Show user by id = #{params[:id]}" \
+		"<form action=\"/users/#{id}\" method=\"POST\">" \
+			'<input type="hidden" name="_method" value="PUT" />' \
+			'<input type="submit" value="Update" />' \
+		'</form>' \
+		"<form action=\"/users/#{id}\" method=\"POST\">" \
+			'<input type="hidden" name="_method" value="DELETE" />' \
+			'<input type="submit" value="Delete" />' \
+		'</form>'
+	end
+
+	put '/users/:id' do
+		"Update user by id = #{params[:id]}"
+	end
+
+	delete '/users/:id' do |id|
+		"Delete user by id = #{id}"
+	end
+
+	get '/hello/:name' do |name|
 		"Hello, #{name}"
 	end
 
-	route '/goodbye' do
+	get '/goodbye' do
 		status 500
 		'Goodbye Cruel World!'
 	end

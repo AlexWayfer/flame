6564bfe5e5bac3c23703bcbca316e70f0633bd6b
Alexander Popov <alex.wayfer@gmail.com>
2015-09-27 23:56:17 +0300

    Remove unused commented code from example app

--- example/my_app.rb
+++ example/my_app.rb
@@ -1,42 +1,10 @@
 ## Test app for Framework
 class MyApp < Flame::Application
-	# get '/' do
-	# 	view :index
-	# end
-	#
-	# get '/users' do
-	# 	view 'users/index'
-	# end
-	#
-	# post '/users' do
-	# 	view 'users/create'
-	# end
-	#
-	# get '/users/:id' do |id|
-	# 	view 'users/show', locals: { id: id }
-	# end
-	#
-	# put '/users/:id' do
-	# 	view 'users/update'
-	# end
-	#
-	# delete '/users/:id' do
-	# 	view 'users/delete'
-	# end
-	#
-	# get '/hello/:name' do |name|
-	# 	view :hello, name: name
-	# end
-	#
-	# get '/goodbye' do
-	# 	status 500
-	# 	'Goodbye Cruel World!'
-	# end
-
 	mount HomeController, '/home' do
 		get '/', :index
 		get '/welcome/:name', :welcome
 	end
+
 	mount UsersController, '/users' do
 		get '/', :index
 		post '/', :create

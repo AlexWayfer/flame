8c32f355cf909017eeff87624251ac1a399add2d
Alexander Popov <alex.wayfer@gmail.com>
2017-02-07 00:15:26 +0300

    Add spec for Version

---
+++ spec/unit/version_spec.rb
@@ -0,0 +1,9 @@
+describe 'Flame::VERSION' do
+	it 'should be String' do
+		Flame::VERSION.should.be.kind_of String
+	end
+
+	it 'should not be empty' do
+		Flame::VERSION.should.not.be.empty
+	end
+end

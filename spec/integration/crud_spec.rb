91d685f637cb3f98c800c114ab040fa7a3da4166
Alexander Popov <alex.wayfer@gmail.com>
2017-02-07 00:16:31 +0300

    Add some integration specs

---
+++ spec/integration/crud_spec.rb
@@ -0,0 +1,61 @@
+require_relative 'app'
+
+## Example of CRUD controller
+class CRUDController < Flame::Controller
+	def index
+		'List of items'
+	end
+
+	def create
+		'Create item'
+	end
+
+	def show(id)
+		"Show item #{id}"
+	end
+
+	def update(id)
+		"Edit item #{id}"
+	end
+
+	def delete(id)
+		"Delete item #{id}"
+	end
+end
+
+## Mount example controller to app
+class MyApp
+	mount CRUDController
+end
+
+describe 'CRUD Controller' do
+	it 'should return list of items' do
+		get '/crud'
+		last_response.should.be.ok
+		last_response.body.should.equal 'List of items'
+	end
+
+	it 'should create item' do
+		post '/crud'
+		last_response.should.be.ok
+		last_response.body.should.equal 'Create item'
+	end
+
+	it 'should show item' do
+		get '/crud/2'
+		last_response.should.be.ok
+		last_response.body.should.equal 'Show item 2'
+	end
+
+	it 'should update item' do
+		put '/crud/4'
+		last_response.should.be.ok
+		last_response.body.should.equal 'Edit item 4'
+	end
+
+	it 'should delete item' do
+		delete '/crud/8'
+		last_response.should.be.ok
+		last_response.body.should.equal 'Delete item 8'
+	end
+end
---
+++ spec/integration/custom_spec.rb
@@ -0,0 +1,80 @@
+require_relative 'app'
+
+## Example of custom controller
+class CustomController < Flame::Controller
+	def foo
+		'This is foo'
+	end
+
+	def hello(name = 'world')
+		"Hello, #{name}!"
+	end
+
+	def page(*path_parts)
+		path_parts.join '/'
+	end
+
+	def error
+		raise StandardError
+	end
+
+	private
+
+	def execute(method)
+		super
+	rescue => exception
+		@rescued = true
+		body default_body
+		raise exception
+	end
+
+	def default_body
+		result = "Some page about #{status} code"
+		result += "; rescued is #{@rescued}" if status == 500
+		result
+	end
+end
+
+## Mount example controller to app
+class MyApp
+	mount CustomController
+end
+
+describe CustomController do
+	it 'should return foo' do
+		get '/custom/foo'
+		last_response.should.be.ok
+		last_response.body.should.equal 'This is foo'
+	end
+
+	it 'should return hello with world' do
+		get '/custom/hello'
+		last_response.should.be.ok
+		last_response.body.should.equal 'Hello, world!'
+	end
+
+	it 'should return hello with name' do
+		get '/custom/hello/Alex'
+		last_response.should.be.ok
+		last_response.body.should.equal 'Hello, Alex!'
+	end
+
+	it 'should return path from array' do
+		# get '/custom/page/path/to/page'
+		# last_response.should.be.ok
+		# last_response.body.should.equal 'path/to/page'
+		should.flunk 'no realization yet'
+	end
+
+	it 'should return custom 404' do
+		get '/custom/foo/404'
+		last_response.should.be.not_found
+		last_response.body.should.equal 'Some page about 404 code'
+	end
+
+	it 'should return custom 500' do
+		get '/custom/error'
+		last_response.should.be.server_error
+		last_response.body.should.equal 'Some page about 500 code; rescued is true'
+	end
+end

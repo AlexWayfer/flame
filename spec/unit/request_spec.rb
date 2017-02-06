a83d55b672e3b2a07401763298d88b069aa7c524
Alexander Popov <alex.wayfer@gmail.com>
2017-02-07 00:15:35 +0300

    Add spec for Request

---
+++ spec/unit/request_spec.rb
@@ -0,0 +1,23 @@
+describe Flame::Request do
+	before do
+		@env = {
+			Rack::REQUEST_METHOD => 'POST',
+			Rack::PATH_INFO => '/hello/great/world',
+			Rack::RACK_INPUT => StringIO.new('_method=PATCH'),
+			Rack::RACK_REQUEST_FORM_HASH => { '_method' => 'PATCH' }
+		}
+		@request = Flame::Request.new(@env)
+	end
+
+	it 'should be Rack::Request child' do
+		@request.should.be.kind_of Rack::Request
+	end
+
+	it 'should split path to parts' do
+		@request.path_parts.should.equal %w(hello great world)
+	end
+
+	it 'should overwrite HTTP-method by parameter' do
+		@request.http_method.should.equal 'PATCH'
+	end
+end

5ac383953500b938f0826c84f82d0143d4c5b3aa
Alexander Popov <alex.wayfer@gmail.com>
2017-03-12 18:39:47 +0300

    Add complete Validators spec

---
+++ spec/unit/validators_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+## Test controller for Validators
+class ValidatorsController < Flame::Controller
+	def foo(first, second, third = nil); end
+end
+
+describe 'Flame::Validators' do
+	describe Flame::Validators::RouteArgumentsValidator do
+		before do
+			@init = proc do |path:|
+				Flame::Validators::RouteArgumentsValidator.new(
+					ErrorsController, path, :foo
+				)
+			end
+		end
+
+		describe '#valid?' do
+			it 'should return true for no extra arguments' do
+				@init.call(path: '/foo/:first/:second/:?third')
+					.valid?.should.equal true
+			end
+
+			it 'should raise error for extra action required arguments' do
+				path = '/foo/:first/:?third'
+				-> { @init.call(path: path).valid? }
+					.should.raise(Flame::Errors::RouterError::RouteArgumentsError)
+					.message.should.equal(
+						"Path '#{path}' has no required arguments [:second]"
+					)
+			end
+
+			it 'should raise error for extra action optional arguments' do
+				path = '/foo/:first/:second'
+				-> { @init.call(path: path).valid? }
+					.should.raise(Flame::Errors::RouterError::RouteArgumentsError)
+					.message.should.equal(
+						"Path '#{path}' has no optional arguments [:third]"
+					)
+			end
+
+			it 'should raise error for extra path required arguments' do
+				-> { @init.call(path: '/foo/:first/:second/:fourth/:?third').valid? }
+					.should.raise(Flame::Errors::RouterError::RouteArgumentsError)
+					.message.should.equal(
+						"Action 'ErrorsController#foo' has no required arguments [:fourth]"
+					)
+			end
+
+			it 'should raise error for extra path optional arguments' do
+				-> { @init.call(path: '/foo/:first/:second/:?third/:?fourth').valid? }
+					.should.raise(Flame::Errors::RouterError::RouteArgumentsError)
+					.message.should.equal(
+						"Action 'ErrorsController#foo' has no optional arguments [:fourth]"
+					)
+			end
+		end
+	end
+end

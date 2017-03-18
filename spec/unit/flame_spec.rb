81c485c52af722908ee7d339aa04e11ae173f033
Alexander Popov <alex.wayfer@gmail.com>
2017-03-18 15:58:14 +0300

    Add full unit spec for Flame requiring

---
+++ spec/unit/flame_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+describe 'Flame' do
+	it 'should require all files when flame required' do
+		Dir[
+			File.join(__dir__, '..', '..', 'lib', 'flame', '**', '*')
+		].each do |file|
+			(require file).should.equal false
+		end
+	end
+end

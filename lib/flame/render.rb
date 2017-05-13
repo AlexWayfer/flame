95dc24803bea318dd2e8b5102c5c264d4b6a2094
Alexander Popov <alex.wayfer@gmail.com>
2017-05-13 17:04:52 +0300

    Improve searching for view file by controller name
    Don't remove extra (logical) 'controller' parts.

--- lib/flame/render.rb
+++ lib/flame/render.rb
@@ -88,7 +88,11 @@ module Flame
 		## Find possible directories for the controller
 		def controller_dirs
 			parts = @controller.class.underscore.split('/').map do |part|
-				(part.split('_') - %w[controller controllers ctrl]).join('_')
+				%w[_controller _ctrl]
+					.find { |suffix| part.chomp! suffix }
+				part
+				## Alternative, but slower by ~50%:
+				# part.sub(/_(controller|ctrl)$/, '')
 			end
 			combine_parts(parts).map! { |path| path.join('/') }
 		end

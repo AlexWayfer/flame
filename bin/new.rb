64be22a65b56e05a5a1d0383a191c82da0f04c77
Alexander Popov <alex.wayfer@gmail.com>
2017-08-08 00:00:54 +0300

    Refactor `flame new` command of bin file

---
+++ bin/new.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'gorilla-patch/inflections'
+require 'erb'
+
+require_relative 'new/app'
+
+module FlameCLI
+	## Command for generating new objects
+	class New < Thor
+		desc 'app APP_NAME', 'Generate new application directory with sub-directories'
+		def app(app_name)
+			self.class::App.new app_name
+		end
+	end
+end
---
+++ spec/flame_cli/spec_helper.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+FLAME_CLI = File.join(__dir__, '..', '..', 'bin', 'flame')

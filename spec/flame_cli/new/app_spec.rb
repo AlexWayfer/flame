4989362de0577a7b1ed48fbb9ce79b090739cffb
Alexander Popov <alex.wayfer@gmail.com>
2017-08-10 00:38:12 +0300

    Fix `flame` gem in template Gemfile

--- spec/flame_cli/new/app_spec.rb
+++ spec/flame_cli/new/app_spec.rb
@@ -108,6 +108,11 @@ describe 'FlameCLI::New::App' do
 	should 'generate working app' do
 		execute_command.call
 		Dir.chdir app_name
+		## HACK for newest unpublished features
+		File.write(
+			'Gemfile',
+			File.read('Gemfile').sub("gem 'flame'\n", "gem 'flame', path: '..'\n")
+		)
 		%w[server].each do |config|
 			FileUtils.cp "config/#{config}.example.yml", "config/#{config}.yml"
 		end
